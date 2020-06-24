fd710f2a53446b9afbd99590388a00aef59cbdb7
pooya lavasani <pooya.lavasani@gmail.com>
2020-06-24 21:16:38 +0430

    make homepage component (also with directory and menu-item components), finish styling menu-item

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Dircetory from "../../components/directory/directory";
+import "./homepage.style.scss";
+
+export default function Homepage() {
+  return (
+    <div className="homepage">
+      <Dircetory />
+    </div>
+  );
+}
